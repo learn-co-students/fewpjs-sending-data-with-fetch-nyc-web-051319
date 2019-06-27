02e7a4b1f97fdd189dfddff8fa5b5e71f0d00e77
Amy Cen <cenamy@gmail.com>
2019-06-27 10:45:11 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+const errorMsg = document.querySelector("#error")
+const userList = document.querySelector("#user-list");
+
+function submitData(userName, userEmail) {
+    fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: userName, email: userEmail})
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        addToDom(object)
+    })
+    .catch(function (error) {
+        errorMsg.innerText = error.message
+    })
+}
+
+function addToDom(obj) {
+    const userItem = document.createElement("li")
+    const itemText = document.createTextNode(`ID: ${obj.id}  Name: ${obj.name}  Email: ${obj.email}`)
+    //userItem.innerText = 
+    userItem.appendChild(itemText)
+    userList.appendChild(userItem)
+}
