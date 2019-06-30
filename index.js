7a0da6f8c8e1cb72493a767c18062067a26bd590
Mendel Stillerman <menstiler@gmail.com>
2019-06-30 02:06:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+const body = document.querySelector('body')
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+  })
+  .then(response => response.json())
+  .then(json => {
+    const p = document.createElement('p')
+    p.innerHTML = json.id
+    body.appendChild(p)
+  })
+  .catch(function(error) {
+    const p = document.createElement('p')
+    p.innerHTML = error.message
+    body.appendChild(p)
+  });
+}
